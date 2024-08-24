ed9f08ed5e2365712038bbdf45542c442c74d4bc
KevinStevenGG00 <150202462+KevinStevenGG00@users.noreply.github.com>
2024-08-23 20:44:44 -0500

    fix: sidebar design - scroll - footer and exit button always visible

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
-import { SidebarRoutes } from "@/components/SidebarRoutes"
-import { Logo } from "@/components/Logo"
+import { SidebarRoutes } from '@/components/SidebarRoutes'
+import { Logo } from '@/components/Logo'
 
 export function Sidebar() {
   return (
-    <div className="h-screen">
-        <div className="h-full flex flex-col border-r">
-            <Logo />
-            <SidebarRoutes />
-        </div>
+    <div className="h-screen flex flex-col border-r">
+      <Logo />
+      <div className="flex-grow overflow-hidden">
+        <SidebarRoutes />
+      </div>
     </div>
   )
 }
--- src/components/SidebarRoutes/SidebarRoutes.tsx
+++ src/components/SidebarRoutes/SidebarRoutes.tsx
@@ -1,21 +1,22 @@
-"use client";
+'use client'
+import { signOut } from 'next-auth/react'
+import { LogOut } from 'lucide-react'
 
-import { Separator } from "@/components/ui/separator";
-import { SidebarItem } from "../SidebarItem";
-import { Button } from "@/components/ui/button";
-import { signOut } from "next-auth/react";
+import { Separator } from '@/components/ui/separator'
+import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
+import { SidebarItem } from '../SidebarItem'
 import {
   dataGeneralSidebar,
   dataSupportSidebar,
   dataToolsSidebar,
-} from "./SidebarRoutes.data";
-import { LogOut } from "lucide-react";
+} from './SidebarRoutes.data'
 
 export function SidebarRoutes() {
   return (
-    <div className="flex flex-col justify-between h-full">
-      <div>
+    <div className="flex flex-col h-full">
+      <ScrollArea className="flex-grow">
         <div className="p-2 md:p-6">
           <p className="text-slate-500 mb-2">GENERAL</p>
           {dataGeneralSidebar.map((item) => (
@@ -36,20 +37,15 @@ export function SidebarRoutes() {
             <SidebarItem key={item.label} item={item} />
           ))}
         </div>
-        <Separator />
-        <div className="p-2 md:p-6">
-          <div className="text-center">
-            <Button onClick={() => signOut()}>
-              Salir <LogOut className="ms-4" width={16} />
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div>
-        <footer className="mt-3 p-3 text-center">
+      </ScrollArea>
+      <div className="p-4 md:p-6 border-t">
+        <Button variant="outline" className="w-full" onClick={() => signOut()}>
+          Salir <LogOut className="ms-4" width={16} />
+        </Button>
+        <footer className="mt-3 text-center text-sm text-gray-500">
           2024. Todos los derechos reservados
         </footer>
       </div>
     </div>
-  );
+  )
 }
