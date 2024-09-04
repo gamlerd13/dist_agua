2a8722aa5c63e82a5ef276474c265fbbc312d29e
PieroS <psulcasanchez@gmail.com>
2024-09-04 03:52:35 -0500

    fix_custom_calendar

--- src/components/Calendario/CustomCalendar.tsx
+++ src/components/Calendario/CustomCalendar.tsx
@@ -4,11 +4,9 @@ import { Calendar as CalendarIcon } from "lucide-react"
 import { format } from "date-fns";
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import { CustomCalendarBase } from "@/components/ui/custom-calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { useEffect, useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { es } from "date-fns/locale"
 
 interface CalendarioScrollableProps {
@@ -50,7 +48,7 @@ export function CustomCalendar({ selected, onSelect }: CalendarioScrollableProps
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
+        <CustomCalendarBase
           mode="single"
           captionLayout="dropdown-buttons"
           selected={selected}
